60821a126d23594184c73df7014e000c698d68b9
Farasha Euker <yourcenar@users.noreply.github.com>
2014-09-21 04:34:05 +0200

    First

---
+++ _plugins/pandoc.rb
@@ -0,0 +1,24 @@
+class Jekyll::Converters::Markdown::Pandoc
+  def initialize(config)
+    require 'pandoc-ruby'
+    @config = config
+  rescue LoadError
+    STDERR.puts 'You are missing a library required for Pandoc. Please run:'
+    STDERR.puts ' $ [sudo] gem install pandoc-ruby'
+    raise FatalException.new("Missing dependency: pandoc-ruby")
+  end
+
+  def convert(content)
+    extensions = config_option('extensions', [])
+    format = config_option('format', 'html5')
+
+    PandocRuby.new(content, *extensions).send("to_#{format}")
+  end
+
+  def config_option(key, default=nil)
+    case @config['pandoc']
+      when nil then default
+      else @config['pandoc'].fetch(key, default)
+    end
+  end
+end
